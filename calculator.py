fa726ee4a679904bf7eff1a42bf1c4e1c5a074d5
Vasyl Deresh <81803610+DeDKiK@users.noreply.github.com>
2024-12-04 00:17:46 +0200

    Update calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+
+def multiply(a, b):
+    c = a*b
+    print(c)
+    return c
+
+
+def add(a, b):
+    add_c = a+b
+    print(add_c)
+    return add_c
+
+
+def substract(a, b):
+    substract_c = a-b
+    print(substract_c)
+    return substract_c
+
+
+def divide(a, b):
+    if b == 0:
+        print("Єбобо?")
+    else:
+        divide_c = a/b
+        print(divide_c)
+    return divide_c
